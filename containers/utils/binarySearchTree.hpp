83d78be96bf81881a5b3b5c15db9325a28abdef1
Slimane CHANCHOUL <schancho@Slimanes-MacBook-Air.local>
2022-04-24 19:52:52 +0000

    update

--- containers/utils/binarySearchTree.hpp
+++ containers/utils/binarySearchTree.hpp
@@ -15,48 +15,48 @@ typedef struct s_node
 class  tree
 {
     public:
-        void insert(int value)
-        {
-            t_node *new_node = new t_node;
-            new_node->data = value;
-            new_node->height = 1;
-            new_node->left = NULL;
-            new_node->right = NULL;
-            new_node->parent = NULL;
-            if(root == NULL)
-            {
-                root = new_node;
-            }
-            else
-            {
-                t_node *current = root;
-                while(current != NULL)
-                {
-                    if(value < current->data)
-                    {
-                        if(current->left == NULL)
-                        {
-                            current->left = new_node;
-                            new_node->parent = current;
-                            break;
-                        }
-                        else
-                            current = current->left;
-                    }
-                    else
-                    {
-                        if(current->right == NULL)
-                        {
-                            current->right = new_node;
-                            new_node->parent = current;
-                            break;
-                        }
-                        else
-                            current = current->right;
-                    }
-                }
-            }
-        }
+        // void insert(int value)
+        // {
+        //     t_node *new_node = new t_node;
+        //     new_node->data = value;
+        //     new_node->height = 1;
+        //     new_node->left = NULL;
+        //     new_node->right = NULL;
+        //     new_node->parent = NULL;
+        //     if(root == NULL)
+        //     {
+        //         root = new_node;
+        //     }
+        //     else
+        //     {
+        //         t_node *current = root;
+        //         while(current != NULL)
+        //         {
+        //             if(value < current->data)
+        //             {
+        //                 if(current->left == NULL)
+        //                 {
+        //                     current->left = new_node;
+        //                     new_node->parent = current;
+        //                     break;
+        //                 }
+        //                 else
+        //                     current = current->left;
+        //             }
+        //             else
+        //             {
+        //                 if(current->right == NULL)
+        //                 {
+        //                     current->right = new_node;
+        //                     new_node->parent = current;
+        //                     break;
+        //                 }
+        //                 else
+        //                     current = current->right;
+        //             }
+        //         }
+        //     }
+        // }
 
         int height(t_node *node)
         {
@@ -113,6 +113,67 @@ class  tree
             y->height = max(height(y->left), height(y->right)) + 1;
 
             return y;
+        }
+
+        t_node *leftRightRotate(t_node *node)
+        {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+
+        t_node *rightLeftRotate(t_node *node)
+        {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+
+        t_node *insert(t_node *node, int value)
+        {
+            if (node == NULL)
+            {
+                t_node *new_node = new t_node;
+                new_node->data = value;
+                new_node->height = 1;
+                new_node->left = NULL;
+                new_node->right = NULL;
+                new_node->parent = NULL;
+                return new_node;
+            }
+
+            if (value < node->data)
+            {
+                node->left = insert(node->left, value);
+                node->left->parent = node;
+            }
+            else if (value > node->data)
+            {
+                node->right = insert(node->right, value);
+                node->right->parent = node;
+            }
+
+            node->height = 1 + max(height(node->left), height(node->right));
+
+            int balance = getBalance(node);
+
+            if (balance > 1 && value < node->left->data)
+                return rightRotate(node);
+
+            if (balance < -1 && value > node->right->data)
+                return leftRotate(node);
+
+            if (balance > 1 && value > node->left->data)
+            {
+                node->left = leftRotate(node->left);
+                return rightRotate(node);
+            }
+
+            if (balance < -1 && value < node->right->data)
+            {
+                node->right = rightRotate(node->right);
+                return leftRotate(node);
+            }
+
+            return node;
         }
     private:
         t_node *node;
