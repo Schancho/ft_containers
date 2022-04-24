1291e0a997929ba97d6efac624ae8171d50305cc
S'limane Chanchoul <schancho@e2r3p9.1337.ma>
2022-04-24 06:54:02 +0000

    update

--- containers/utils/binarySearchTree.hpp
+++ containers/utils/binarySearchTree.hpp
@@ -8,6 +8,8 @@ typedef struct s_node
     s_node *left;
     s_node *right;
     s_node *parent;
+    int height;
+    
 }t_node;
 
 class  tree
@@ -17,6 +19,7 @@ class  tree
         {
             t_node *new_node = new t_node;
             new_node->data = value;
+            new_node->height = 1;
             new_node->left = NULL;
             new_node->right = NULL;
             new_node->parent = NULL;
@@ -38,9 +41,7 @@ class  tree
                             break;
                         }
                         else
-                        {
                             current = current->left;
-                        }
                     }
                     else
                     {
@@ -51,15 +52,71 @@ class  tree
                             break;
                         }
                         else
-                        {
                             current = current->right;
-                        }
                     }
                 }
             }
         }
+
+        int height(t_node *node)
+        {
+            if (node == NULL)
+                return 0;
+            return (node->height);
+        }
+
+        int max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+
+        int getBalance(t_node *node)
+        {
+            if (node == NULL)
+                return 0;
+            return (height(node->left) - height(node->right));
+        }
+
+        t_node *rightRotate(t_node *y)
+        {
+            t_node *x = y->left;
+            t_node *T2 = x->right;
+
+            x->right = y;
+            y->left = T2;
+
+            if (T2 != NULL)
+                T2->parent = y;
+
+            y->parent = x;
+
+            y->height = max(height(y->left), height(y->right)) + 1;
+            x->height = max(height(x->left), height(x->right)) + 1;
+
+            return x;
+        }
+
+        t_node *leftRotate(t_node *x)
+        {
+            t_node *y = x->right;
+            t_node *T2 = y->left;
+
+            y->left = x;
+            x->right = T2;
+
+            if (T2 != NULL)
+                T2->parent = x;
+
+            x->parent = y;
+
+            x->height = max(height(x->left), height(x->right)) + 1;
+            y->height = max(height(y->left), height(y->right)) + 1;
+
+            return y;
+        }
     private:
         t_node *node;
         t_node *root;
+
         
 };
